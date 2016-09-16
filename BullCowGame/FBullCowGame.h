50582d98d54db6c218481bd6925fac88fd2251d4
Denis Ivanko <denis.ivanko11@gmail.com>
2016-09-16 13:50:40 +0300

    GetHiddenWordLength method added

--- BullCowGame/FBullCowGame.h
+++ BullCowGame/FBullCowGame.h
@@ -16,13 +16,14 @@ public:
 	FBullCowGame(); // Constructor
 	int32 GetMaxTries() const; 
 	int32 GetCurrentTry() const;
+	int32 GetHiddenWordLength() const;
+
 	bool IsGameWon() const;
+	bool CheckGuessValidity(FString) const; // TODO make a more rich return value
 
 	void Reset(); // TODO make a more rich return value
-	bool CheckGuessValidity(FString); // TODO make a more rich return value
 	FBullCowCount SubmitGuess(FString);
-
-
+	
 private:
 	// See constructor for initilization
 	int32 MyCurrentTry;
